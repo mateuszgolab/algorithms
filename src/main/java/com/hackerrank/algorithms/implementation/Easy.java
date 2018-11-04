0e7fbe2b506349633f8b90b8d067a7e6e587c92a
mateuszgolab <mathgol@gmail.com>
2018-11-04 13:21:40 +0000

    hackerrank easy impl

--- src/main/java/com/hackerrank/algorithms/implementation/Easy.java
+++ src/main/java/com/hackerrank/algorithms/implementation/Easy.java
@@ -1,6 +1,8 @@
 package com.hackerrank.algorithms.implementation;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -81,6 +83,81 @@ class Easy {
         }
 
         return price;
+    }
+
+    static String catAndMouse(int x, int y, int z) {
+
+        int catA = Math.abs(z - x);
+        int catB = Math.abs(z - y);
+
+        if (catA < catB) {
+            return "Cat A";
+        } else if (catB < catA) {
+            return "Cat B";
+        } else {
+            return "Mouse C";
+        }
+
+    }
+
+    static int pickingNumbers(List<Integer> a) {
+
+        Collections.sort(a);
+        int max = 0;
+
+        for (int i = 0; i < a.size() - 1; i++) {
+            int tmp = 1;
+            for (int j = i + 1; j < a.size(); j++) {
+                if (Math.abs(a.get(i) - a.get(j)) < 2) tmp++;
+                else break;
+            }
+
+            if (tmp > max) max = tmp;
+
+        }
+
+        return max;
+
+
+    }
+
+    static int hurdleRace(int k, int[] height) {
+        return Math.max(Arrays.stream(height).max().getAsInt() - k, 0);
+    }
+
+    static int designerPdfViewer(int[] h, String word) {
+
+        int max = 0;
+        for (Character ch : word.toCharArray()) {
+            int index = ch - (int) 'a';
+            max = Math.max(max, h[index]);
+        }
+
+        return word.length() * max;
+
+    }
+
+    static int utopianTree(int n) {
+
+        int result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) result *= 2;
+            else result++;
+        }
+
+        return result;
+
+    }
+
+    static String angryProfessor(int k, int[] a) {
+
+        if (Arrays.stream(a).filter(v -> v <= 0).count() >= k) {
+            return "NO";
+        } else {
+            return "YES";
+        }
+
     }
 
 
--- src/test/java/com/hackerrank/algorithms/implementation/EasyTest.java
+++ src/test/java/com/hackerrank/algorithms/implementation/EasyTest.java
@@ -2,6 +2,8 @@ package com.hackerrank.algorithms.implementation;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static com.hackerrank.algorithms.implementation.Easy.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -32,13 +34,13 @@ public class EasyTest {
     @Test
     public void drawingBookTest() {
 
-        assertThat(pageCount(5,4), is(0));
-        assertThat(pageCount(6,2), is(1));
-        assertThat(pageCount(10,5), is(2));
-        assertThat(pageCount(10,6), is(2));
-        assertThat(pageCount(10,9), is(1));
-        assertThat(pageCount(10,10), is(0));
-        assertThat(pageCount(10,1), is(0));
+        assertThat(pageCount(5, 4), is(0));
+        assertThat(pageCount(6, 2), is(1));
+        assertThat(pageCount(10, 5), is(2));
+        assertThat(pageCount(10, 6), is(2));
+        assertThat(pageCount(10, 9), is(1));
+        assertThat(pageCount(10, 10), is(0));
+        assertThat(pageCount(10, 1), is(0));
 
     }
 
@@ -52,10 +54,64 @@ public class EasyTest {
     @Test
     public void electronicShopTest() {
 
-        assertThat(getMoneySpent(new int[]{3,1}, new int[]{5,2,8}, 10), is(9));
+        assertThat(getMoneySpent(new int[]{3, 1}, new int[]{5, 2, 8}, 10), is(9));
         assertThat(getMoneySpent(new int[]{4}, new int[]{5}, 5), is(-1));
-        assertThat(getMoneySpent(new int[]{4,8,10}, new int[]{5,10,20}, 15), is(15));
-        assertThat(getMoneySpent(new int[]{4,8,10}, new int[]{5,10,20}, 24), is(24));
+        assertThat(getMoneySpent(new int[]{4, 8, 10}, new int[]{5, 10, 20}, 15), is(15));
+        assertThat(getMoneySpent(new int[]{4, 8, 10}, new int[]{5, 10, 20}, 24), is(24));
+    }
+
+    @Test
+    public void catAndAMouseTest() {
+
+        assertThat(catAndMouse(1, 2, 3), is("Cat B"));
+        assertThat(catAndMouse(1, 3, 2), is("Mouse C"));
+        assertThat(catAndMouse(1, 6, 2), is("Cat A"));
+
+    }
+
+    @Test
+    public void pickingNumbersTest() {
+
+        assertThat(pickingNumbers(Arrays.asList(4,6,5,3,3,1)), is(3));
+        assertThat(pickingNumbers(Arrays.asList(1,2,2,3,1,2)), is(5));
+        assertThat(pickingNumbers(Arrays.asList(1,2,2,3,3,5)), is(4));
+
+    }
+
+
+    @Test
+    public void theHurdleRaceTest() {
+
+        assertThat(hurdleRace(4, new int[]{1,6,3,5,2}), is(2));
+        assertThat(hurdleRace(7, new int[]{2,5,4,5,2}), is(0));
+
+    }
+
+    @Test
+    public void designPDFViewerTest() {
+
+        assertThat(designerPdfViewer(new int[]{1,3,1,3,1,4,1,3,2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5}, "abc"), is(9));
+        assertThat(designerPdfViewer(new int[]{1,3,1,3,1,4,1,3,2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,7}, "zaba"), is(28));
+
+    }
+
+    @Test
+    public void utopianTreeTest() {
+
+        assertThat(utopianTree(0), is(1));
+        assertThat(utopianTree(1), is(2));
+        assertThat(utopianTree(4), is(7));
+        assertThat(utopianTree(5), is(14));
+        assertThat(utopianTree(3), is(6));
+
+    }
+
+    @Test
+    public void angryProfessorTest() {
+
+        assertThat(angryProfessor(3, new int[]{-1,-3,4,2}), is("YES"));
+        assertThat(angryProfessor(2, new int[]{0,-1,2,1}), is("NO"));
+
     }
 
 }
