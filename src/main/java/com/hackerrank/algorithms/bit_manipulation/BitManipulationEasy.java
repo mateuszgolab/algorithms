4e77ee9994750268fc2dfb194df5d6e9149febd9
mateuszgolab <mathgol@gmail.com>
2019-04-03 21:19:05 +0100

    + hackerrank easy bit manipulation

--- src/main/java/com/hackerrank/algorithms/bit_manipulation/BitManipulationEasy.java
+++ src/main/java/com/hackerrank/algorithms/bit_manipulation/BitManipulationEasy.java
@@ -5,20 +5,33 @@ import java.util.Arrays;
 class BitManipulationEasy {
 
     static int lonelyinteger(int[] a) {
-        return Arrays.stream(a).reduce(0, (x, y) -> x^y);
+        return Arrays.stream(a).reduce(0, (x, y) -> x ^ y);
     }
 
     static int maximizingXor(int l, int r) {
 
         int max = 0;
 
-        for(int i = l; i <= r; i++){
-            for(int j = i+1; j <= r; j++){
-                max = Math.max(max, i ^ j );
+        for (int i = l; i <= r; i++) {
+            for (int j = i + 1; j <= r; j++) {
+                max = Math.max(max, i ^ j);
             }
         }
 
         return max;
+
+    }
+
+    static long sumXor(long n) {
+
+        if(n == 0) return 1;
+
+        long numOfZeroes = Long.toBinaryString(n).
+                chars().
+                filter(i -> (char) i == '0').
+                count();
+
+        return (long) Math.pow(2, numOfZeroes);
 
     }
 
--- src/test/java/com/hackerrank/algorithms/bit_manipulation/BitManipulationEasyTest.java
+++ src/test/java/com/hackerrank/algorithms/bit_manipulation/BitManipulationEasyTest.java
@@ -2,8 +2,7 @@ package com.hackerrank.algorithms.bit_manipulation;
 
 import org.junit.Test;
 
-import static com.hackerrank.algorithms.bit_manipulation.BitManipulationEasy.lonelyinteger;
-import static com.hackerrank.algorithms.bit_manipulation.BitManipulationEasy.maximizingXor;
+import static com.hackerrank.algorithms.bit_manipulation.BitManipulationEasy.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -25,6 +24,20 @@ public class BitManipulationEasyTest {
         assertThat(maximizingXor(11,12), is(7));
         assertThat(maximizingXor(10,15), is(7));
         assertThat(maximizingXor(11,100), is(127));
+
+    }
+
+    @Test
+    public void testSumVsXor() {
+
+        assertThat(sumXor(4), is(4L));
+        assertThat(sumXor(5), is(2L));
+        assertThat(sumXor(10), is(4L));
+        assertThat(sumXor(0), is(1L));
+        assertThat(sumXor(1), is(1L));
+        assertThat(sumXor(2), is(2L));
+        assertThat(sumXor(8), is(8L));
+        assertThat(sumXor(128), is(128L));
 
     }
 
